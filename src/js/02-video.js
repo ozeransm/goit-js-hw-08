57ebcdcfecbd9c406ad0eaa65e1e1858300013fb
Serhii Ozeran <ozeransm@gmail.com>
2023-05-04 21:38:26 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
 
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+// localStorage.clear();
+if(!localStorage.getItem("videoplayer-current-time")){
+    player.on('timeupdate', throttle((data)=>{
+        
+        const {seconds} = data;
+        localStorage.setItem("videoplayer-current-time", seconds);
+    },1000));
+}else {
+    
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+                // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+
+    player.on('timeupdate', throttle((data)=>{
+        const {seconds} = data;
+        localStorage.setItem("videoplayer-current-time", seconds);
+    },1000));
+
+}
